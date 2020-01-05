541179356aa97a29ad1b3e377f8fe9e96c572213
HadiSheybani <hadi.sheybani1993@hotmail.com>
2020-01-05 13:28:26 +0330

    a fail test has been fixed

--- Phonebook/Tests/adapter/test_user_data_access.py
+++ Phonebook/Tests/adapter/test_user_data_access.py
@@ -36,17 +36,18 @@ class TestUserDataAccess:
         test_user_email = 'test@email.com'
         test_user_password = '123456'
 
-        user = User(test_user_name
-                    , test_user_email
-                    , test_user_email
-                    , test_user_password)
-        self.__query_builder.get_user_query.return_value = json.dumps({"name": user.name
-                                                                , "email": user.email
-                                                                , "username": user.email
-                                                                , "pass_hash": user.pass_hash
-                                                                , "contacts": []})
-        self.__database.execute.return_value = user
+        user_data = '{"name": "hadi", "email": "test@email.com", "username": "test@email.com", "pass_hash": "123456"}'
+        user = User("hadi",
+                    "test@email.com",
+                    "test@email.com",
+                    "123456")
+        self.__query_builder.get_user_query.return_value = json.dumps({"username": test_user_email})
+        self.__database.execute.return_value = user_data
         user_data_access = UserDataAccess(self.__query_builder, self.__database)
         output_user = user_data_access.get(test_user_email)
         self.__database.execute.assert_called_once_with(self.__query_builder.get_user_query.return_value)
-        assert_that(output_user, equal_to(user))
+
+        assert_that(output_user.name, equal_to(user.name))
+        assert_that(output_user.email, equal_to(user.email))
+        assert_that(output_user.username, equal_to(user.username))
+        assert_that(output_user.pass_hash, equal_to(user.pass_hash))
